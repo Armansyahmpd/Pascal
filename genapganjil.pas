306b7eb89e590195380cf258e301635a41e957c5
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2022-10-29 10:15:03 +0700

    Create genapganjil.pas

---
+++ genapganjil.pas
@@ -0,0 +1,13 @@
+Program ganjil_genap; 
+Uses Crt; 
+Var x: integer; 
+
+Begin
+   Write(‘Masukkan angka:’); 
+   Readln(x); 
+     
+     If x mod 2 = 0 then 
+     Write(‘Bilangan Genap’) 
+       Else 
+     Write(‘Bilangan Ganjil’); 
+End.
